23448227adf61905e19e2244b74829e2e0c0ac81
CT Hung <skycloud1030@gmail.com>
2020-04-23 10:51:32 +0800

    Fix webpack4 umd module window issue

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,19 @@ var plugins = [];
 
 var config = {
   entry: {
-    "index": path.resolve(__dirname, "index.js")
+    index: path.resolve(__dirname, "index.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
     library: "ReactSelectlist",
     libraryTarget: "umd",
-    umdNamedDefine: true
+    umdNamedDefine: true,
+    globalObject: `(typeof self !== 'undefined' ? self : this)`,
   },
   externals: {
     react: "react",
-    "react-dom": "react-dom"
+    "react-dom": "react-dom",
   },
   plugins: plugins,
   resolve: { alias: {} },
@@ -26,38 +27,37 @@ var config = {
         test: /\.js[x]?$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        query: {
-          cacheDirectory: true,
-          presets: ["@babel/preset-env", "@babel/preset-react"]
-        }
+        query: { cacheDirectory: true },
       },
       {
         test: /\.css$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "style-loader!css-loader"
+        loader: "style-loader!css-loader",
       },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: "url-loader?name=[path][name].[ext]&limit=50000"
+        loader: "url-loader?name=[path][name].[ext]&limit=50000",
       }, // inline base64 URLs for <=8k images, direct URLs for the rest
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=[path][name].[ext]"
+        loader: "file-loader?name=[path][name].[ext]",
       },
       {
         test: /\.(woff|woff2)$/,
-        loader: "url-loader?name=[path][name].[ext]&prefix=font/&limit=50000"
+        loader: "url-loader?name=[path][name].[ext]&prefix=font/&limit=50000",
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?name=[path][name].[ext]&limit=50000&mimetype=application/octet-stream"
+        loader:
+          "url-loader?name=[path][name].[ext]&limit=50000&mimetype=application/octet-stream",
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml"
-      }
-    ]
-  }
+        loader:
+          "url-loader?name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml",
+      },
+    ],
+  },
 };
 
 module.exports = config;
